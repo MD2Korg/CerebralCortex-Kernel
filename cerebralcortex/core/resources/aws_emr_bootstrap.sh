9d0b2539fe006f6ca423ffea0158f8025b4a8034
nasirali1 <nasir.ali08@gmail.com>
2018-07-13 10:20:23 -0500

    added test to read/write aws s3 data

--- cerebralcortex/core/resources/aws_emr_bootstrap.sh
+++ cerebralcortex/core/resources/aws_emr_bootstrap.sh
@@ -8,7 +8,7 @@ sudo yum install -y python36-devel
 # install git
 sudo yum install -y git
 
-# Install pythong packages
+# Install pythong packages - CerebralCortex
 sudo pip-3.6 install --egg mysql-connector-python-rf
 sudo pip-3.6 install wheel==0.29.0
 sudo pip-3.6 install pytz==2017.2
@@ -20,5 +20,21 @@ sudo pip-3.6 install pyarrow==0.8.0
 sudo pip-3.6 install pympler==0.5
 sudo pip-3.6 install hdfs3==0.3.0
 
+# Install pythong packages - CerebralCortex-DataAnalysis
+sudo pip-3.6 install pandas
+sudo pip-3.6 install geopy
+sudo pip-3.6 install scikit-learn
+sudo pip-3.6 install numpy
+sudo pip-3.6 install scipy
+sudo pip-3.6 install pympler
+sudo pip-3.6 install shapely
+sudo pip-3.6 install bs4
+sudo pip-3.6 install sphinx
+sudo pip-3.6 install python-google-places
+sudo pip-3.6 install keras
+sudo pip-3.6 install tensorflow
+sudo pip-3.6 install bs4
+
 # copy cc config file to home dir
 aws s3 cp s3://cerebralcortex-resources/cc_config/cc_aws_config.yml ~/
+aws s3 cp s3://cerebralcortex-resources/scripts/prepare_cc.sh ~/
--- cerebralcortex/core/test_suite/spark_test.py
+++ cerebralcortex/core/test_suite/spark_test.py
@@ -3,6 +3,7 @@
 import time
 from cerebralcortex.cerebralcortex import CerebralCortex
 from cerebralcortex.core.util.spark_helper import get_or_create_sc
+import uuid
 
 cc_config = "/home/hadoop/cc_aws_config.yml"
 CC = CerebralCortex(cc_config)
@@ -14,16 +15,30 @@ def example_method(user_id, cc_config):
     print("User ID:", user_id, "User Name:", user_name)
     time.sleep(1)
 
-study_name = "mperf"
-all_users = CC.get_all_users(study_name)
+def run_spark_job():
+    study_name = "mperf"
+    all_users = CC.get_all_users(study_name)
 
-if all_users:
-    rdd = spark_context.parallelize(all_users)
-    rdd = rdd.repartition(200)
-    results = rdd.map(
-        lambda user: example_method(user["identifier"], cc_config))
-    results.count()
-else:
-    print(study_name, "- study has 0 users.")
+    if all_users:
+        rdd = spark_context.parallelize(all_users)
+        rdd = rdd.repartition(200)
+        results = rdd.map(
+            lambda user: example_method(user["identifier"], cc_config))
+        results.count()
+    else:
+        print(study_name, "- study has 0 users.")
 
+def aws_s3_read_write():
+    ds = CC.get_stream("5b5d304d-3588-369e-ad68-d839d7718495","dd13f25f-77a0-4a2c-83af-bb187b79a389","20171226")
+    print(ds.data[:5])
+
+    ds._identifier = str(uuid.UUID('{00000000-5087-3d56-ad0e-0b27c3c83182}'))
+    ds._owner = str(uuid.UUID('{00000000-f81c-44d2-9db8-fea69f468d58}'))
+    CC.save_stream(ds)
+
+# test spark
+#run_spark_job()
+
+# test read/write aws-s3
+aws_s3_read_write()
 
