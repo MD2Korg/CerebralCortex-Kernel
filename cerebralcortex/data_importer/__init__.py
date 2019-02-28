eb523c133d4a666ecf99cd08b1deceeadb831655
nasirali1 <nasir.ali08@gmail.com>
2019-02-27 23:05:57 -0600

    renamed file to avoid ambiguity

--- cerebralcortex/data_importer/__init__.py
+++ cerebralcortex/data_importer/__init__.py
@@ -1,3 +1,3 @@
-from cerebralcortex.data_importer.data_importer import import_file, import_dir
+from cerebralcortex.data_importer.ingest import import_file, import_dir
 
 __all__= ["import_file", "import_dir"]
--- cerebralcortex/data_importer/ingest.py
+++ cerebralcortex/data_importer/ingest.py
@@ -338,17 +338,17 @@ def import_dir(cc_config: dict, input_data_dir: str, user_id: str = None, data_f
         print_stats_table(CC.SqlData.get_ingestion_stats())
 
 
-from cerebralcortex.data_importer.data_parsers import mcerebrum_data_parser
-
-import_dir(
-    cc_config="/home/ali/IdeaProjects/CerebralCortex-2.0/conf/",
-    input_data_dir="/home/ali/IdeaProjects/MD2K_DATA/data/test/",
-    batch_size=20,
-    compression='gzip',
-    header=None,
-    data_file_extension=[".gz"],
-    # allowed_filename_pattern="REGEX PATTERN",
-    data_parser=mcerebrum_data_parser,
-    metadata_parser=parse_mcerebrum_metadata,
-    gen_report=True
-)
+# from cerebralcortex.data_importer.data_parsers import mcerebrum_data_parser
+#
+# import_dir(
+#     cc_config="/home/ali/IdeaProjects/CerebralCortex-2.0/conf/",
+#     input_data_dir="/home/ali/IdeaProjects/MD2K_DATA/data/test/",
+#     batch_size=20,
+#     compression='gzip',
+#     header=None,
+#     data_file_extension=[".gz"],
+#     # allowed_filename_pattern="REGEX PATTERN",
+#     data_parser=mcerebrum_data_parser,
+#     metadata_parser=parse_mcerebrum_metadata,
+#     gen_report=True
+# )
