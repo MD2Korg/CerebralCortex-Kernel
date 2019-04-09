f207d00cc4ad431cdc42bcf40160f8a0d0dee700
Anand Nandugudi <nndugudi@memphis.edu>
2019-04-09 11:15:59 -0500

    added a test algorithm to play with the API

--- cerebralcortex/algorithms/__init__.py
+++ cerebralcortex/algorithms/__init__.py
@@ -2,4 +2,5 @@ from cerebralcortex.algorithms.gps import gps_clusters
 from cerebralcortex.algorithms.ecg.ecg_signal_processing import process_ecg
 from cerebralcortex.algorithms.stress_prediction.stress_prediction import stress_prediction
 from cerebralcortex.algorithms.rr_intervals.rr_interval_feature_extraction import rr_interval_feature_extraction
-__all__ = ["gps_clusters","process_ecg", "rr_interval_feature_extraction", "stress_prediciton"]
+from cerebralcortex.algorithms.test.test import test_window
+__all__ = ["gps_clusters","process_ecg", "rr_interval_feature_extraction", "stress_prediciton", "test_window"]
