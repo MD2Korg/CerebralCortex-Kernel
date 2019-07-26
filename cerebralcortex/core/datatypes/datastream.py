5d43b044d151f5f42aa794089b646662b73a2231
nasirali1 <nasir.ali08@gmail.com>
2019-07-25 21:46:16 -0500

    remove columns if exist for plotting

--- cerebralcortex/core/datatypes/datastream.py
+++ cerebralcortex/core/datatypes/datastream.py
@@ -475,7 +475,7 @@ class DataStream:
 
         tmp = "{}{}{}{}".format(str("udfName.__name__"), "(",tmp.rstrip(","), ")")
         tt = eval(tmp)
-        foobars = self._data.groupBy(groupbycols).agg(F.expr('find_a(collect_list("some_vals"))').alias("foobar"))
+        foobars = self._data.groupBy(groupbycols).agg(F.expr('find_a(F.collect_list("some_vals"))').alias("foobar"))
         cols = foobars.schema.fields
         new_cols = []
         for col in cols:
