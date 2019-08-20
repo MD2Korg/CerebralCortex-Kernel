2a8394355132d97b65527a89c002e17b4950cb87
nasirali1 <nasir.ali08@gmail.com>
2019-08-20 14:18:01 -0500

    fixed bug CC-653 all times should be in UTC

--- cerebralcortex/core/util/spark_helper.py
+++ cerebralcortex/core/util/spark_helper.py
@@ -45,6 +45,8 @@ def get_or_create_sc(type="sparkContext", name="CerebralCortex-Kernal", enable_s
     ss.config("spark.streaming.backpressure.enabled", True)
     ss.config("spark.streaming.backpressure.initialRate", 1)
     ss.config("spark.streaming.kafka.maxRatePerPartition", 2)
+    ss.config("spark.sql.session.timeZone", "UTC")
+
     if enable_spark_ui==False:
         ss.config("spark.ui.enabled", enable_spark_ui)
 
