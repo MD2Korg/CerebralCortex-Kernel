aa563da92881f7a173827c7308b94cd388fac6be
nasirali1 <nasir.ali08@gmail.com>
2019-11-04 17:05:35 -0600

    updated cc version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ with open(path.join(here, 'README.md')) as f:
 setup(
     name="cerebralcortex-kernel",
 
-    version='3.1.0r3',
+    version='3.1.0r4',
 
     description='Backend data analytics platform for MD2K software',
     long_description=long_description,
