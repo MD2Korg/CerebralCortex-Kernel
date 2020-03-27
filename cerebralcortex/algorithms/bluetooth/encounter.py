df80f9501d2e7551092fba268cd5005a76f138a0
aungkonazim <aungkonazim@gmail.com>
2020-03-27 12:20:32 -0500

    added bluetooth encounter

--- cerebralcortex/algorithms/bluetooth/encounter.py
+++ cerebralcortex/algorithms/bluetooth/encounter.py
@@ -1,5 +1,5 @@
 # Copyright (c) 2019, MD2K Center of Excellence
-# - Md Azim Ullah <aungkonazim@gmail.com>
+# - Md Azim Ullah <mullah@memphis.edu>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -24,23 +24,28 @@
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
-from pyspark.sql.types import StructField, StructType, StringType, FloatType, BooleanType, DoubleType,StringType,FloatType, TimestampType, IntegerType
+from pyspark.sql.types import StructField, StructType, DoubleType,StringType, TimestampType, IntegerType
 from pyspark.sql.functions import pandas_udf, PandasUDFType
 from pyspark.sql import functions as F
 from cerebralcortex.core.datatypes import DataStream
-from cerebralcortex.core.metadata_manager.stream.metadata import Metadata, DataDescriptor, ModuleMetadata
-import pandas as pd, numpy as np, os, csv, glob
+from cerebralcortex.core.metadata_manager.stream.metadata import Metadata
+import pandas as pd, numpy as np
+from datetime import datetime
 
 
-def get_bluetooth_encounter(data,
-                            distance_threshold=5,
-                            count_threshold = 5,
-                            n_rows_threshold = 2,
-                            time_threshold=5*60,
-                            epsilon = 1e-3):
+def bluetooth_encounter(data,
+                        st:datetime,
+                        et:datetime,
+                        distance_threshold=5,
+                        average_count_threshold = 20,
+                        n_rows_threshold = 2,
+                        time_threshold=5*60,
+                        epsilon = 1e-3):
     """
 
     :param ds: Input Datastream
+    :param st: Start Time the time window in UTC
+    :param et: End Time of time window in UTC
     :param distance_threshold: Threshold on mean distance per encounter
     :param n_rows_threshold: No of rows per group/encounter
     :param time_threshold: Minimum Duration of time per encounter
@@ -48,10 +53,74 @@ def get_bluetooth_encounter(data,
     :param count_threshold: Threshold on count
     :return: A Sparse representation of the Bluetooth Encounter
     """
+    def get_data(data,i,k,distance_threshold):
+        distances = data['distance_estimate'].values[k:i]
+        rssi = data['RSSI'].values[k:i]
+        weights1 = rssi  + np.abs(np.min(rssi)) + epsilon
+        weights2 = 100*(distances-np.min(distances)) + epsilon
+        weights = 1/(weights1+weights2)
+        mean_distance = np.average(distances,weights=weights)
+        if mean_distance <= distance_threshold:
+            return [data['user'].iloc[k],data['participant_identifier'].iloc[k],data['localtime'].iloc[k],
+                    data['localtime'].iloc[i-1],data['version'].iloc[k],mean_distance,data['timestamp'].iloc[int((i+k)/2)],
+                    data['localtime'].iloc[int((i+k)/2)],np.mean(data['latitude'].values[k:i]),
+                    np.mean(data['longitude'].values[k:i]),data['indicator'].iloc[k],np.mean(data['count'].values[k:i])]
+        return []
 
-    data['time'] = data['time'].apply(lambda a:datetime.fromtimestamp(a))
-    data['timestamp'] = data['time'].apply(lambda d:d.timestamp())
-    data['hour'] = data['time'].apply(lambda d:d.hour)
-    data['day'] = data['time'].apply(lambda d:d.strftime('%Y%m%d'))
+    schema = StructType([StructField('timestamp', TimestampType()),
+                         StructField('localtime', TimestampType()),
+                         StructField('start_time', TimestampType()),
+                         StructField('end_time', TimestampType()),
+                         StructField('user', StringType()),
+                         StructField('participant_identifier', StringType()),
+                         StructField('version', IntegerType()),
+                         StructField('indicator', StringType()),
+                         StructField('latitude', DoubleType()),
+                         StructField('mean_distance', DoubleType()),
+                         StructField('longitude', DoubleType()),
+                         StructField('average_count', DoubleType())
+                         ])
+
+    @pandas_udf(schema, PandasUDFType.GROUPED_MAP)
+    def get_enconters(data):
+        if data.shape[0]<n_rows_threshold:
+            return pd.DataFrame([],columns = ['user','participant_identifier','start_time','end_time','version',
+                                              'mean_distance','timestamp','localtime','latitude','longitude','indicator','average_count'])
+        data = data.sort_values('timestamp').reset_index(drop=True)
+        data_all = []
+        for i,row in data.iterrows():
+            if i==0:
+                k = int(i)
+                start_time = row['time']
+                count = 0
+            else:
+                if row['time'] - start_time < time_threshold/2:
+                    start_time = row['time']
+                    count+=1
+                else:
+                    if i-k>n_rows_threshold and data['time'].iloc[i]-data['time'].iloc[k]>time_threshold and np.mean(data['count'].values[k:i])>average_count_threshold:
+                        temp = get_data(data,i,k,distance_threshold)
+                        if len(temp)>0:
+                            data_all.append(temp)
+                    count = 0
+                    k = i
+                    start_time = row['time']
+        if count>0:
+            i = data.shape[0]-1
+            temp = get_data(data,i,k,distance_threshold)
+            if len(temp)>0:
+                data_all.append(temp)
+
+        return pd.DataFrame(data_all,columns = ['user','participant_identifier','start_time','end_time','version',
+                                                'mean_distance','timestamp','localtime','latitude','longitude','indicator','average_count'])
+
+
+
+    data = data.withColumn('time',F.col('timestamp').cast('double'))
+    data_filtered = data._data.filter((data.timestamp>=st) & (data.timestamp<et))
+    data_result = data_filtered.groupBy(['user','participant_identifier']).apply(get_enconters)
+    return DataStream(data=data_result, metadata=Metadata())
+
+
 
 
