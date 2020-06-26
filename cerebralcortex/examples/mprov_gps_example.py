a398aa3c7b27d7450742c532e20f9e0f582cc66f
nasirali1 <nasir.ali08@gmail.com>
2020-06-25 21:39:04 -0500

    enable graph name for now

--- cerebralcortex/examples/mprov_gps_example.py
+++ cerebralcortex/examples/mprov_gps_example.py
@@ -3,7 +3,7 @@ from cerebralcortex.algorithms.gps.clustering import cluster_gps
 from cerebralcortex.kernel import Kernel
 
 
-CC = Kernel(cc_configs="default", mprov=False)
+CC = Kernel(cc_configs="default", mprov=True)
 ds_gps = gen_location_datastream(user_id="bfb2ca0c-e19c-3956-9db2-5459ccadd40c", stream_name="gps--org.md2k.phonesensor--phone")
 
 d2=ds_gps.window(windowDuration=60)
